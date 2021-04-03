79b6599259ea4827c3f21830113ac24c6fae1b27
HeraclitoDeEfeso <eloscurodeefeso@gmail.com>
2021-04-03 16:23:08 -0300

    Opciones para rapida compilacion

--- make-nb.bat
+++ make-nb.bat
@@ -1,3 +1,5 @@
-jupyter nbconvert --to=latex --template-file=%2 --Preprocessor.enabled=True --ConvertFiguresPreprocessor.display_data_priority=['image/png'] --ConvertFiguresPreprocessor.enabled=True %1
-pdflatex %~n1.tex
-pdflatex %~n1.tex
+echo off
+jupyter nbconvert --to=latex --template-file=%2 --Preprocessor.enabled=True  --ConvertFiguresPreprocessor.enabled=True --NbConvertBase.display_data_priority 'image/png' %1
+pdflatex %~n1.tex -interaction=batchmode -draftmode -no-pdf
+pdflatex %~n1.tex -interaction=batchmode
+echo on
