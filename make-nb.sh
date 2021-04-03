3a0e78a7274092172a7be611fd04efd0b32eda55
HeraclitoDeEfeso <eloscurodeefeso@gmail.com>
2021-04-03 16:48:39 -0300

    Options faster compilation. Echo management

--- make-nb.sh
+++ make-nb.sh
@@ -1,5 +1,5 @@
 echo off
 jupyter nbconvert --to=latex --template-file=$2 --Preprocessor.enabled=True --ConvertFiguresPreprocessor.enabled=True --ConvertFiguresPreprocessor.display_data_priority 'image/png' $1
-fn=${1%.*}
-pdflatex $fn.tex
-pdflatex $fn.tex
+pdflatex ${1%.*}.tex -interaction=batchmode -draftmode -no-pdf
+pdflatex ${1%.*}.tex -interaction=batchmode
+echo on
